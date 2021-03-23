49c9d7b4db517c0c22ecc23c6be7ae1010ad1bec
Shehan Wijeyaweera <shehan.naveendra@gmail.com>
2021-03-24 01:06:41 +0530

    implemented template engine Ejs

--- routes/shop.js
+++ routes/shop.js
@@ -1,4 +1,4 @@
-const path = require('path')
+const path = require('path');
 
 const express = require('express');
 
@@ -8,9 +8,15 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    const products = adminData.products;
-    //res.sendFile(path.join(rootDir ,'views' , 'shop.html'));
-    res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
